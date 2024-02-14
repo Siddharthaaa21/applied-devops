25c2731127ea653badac9b6cc637eb96e73e4964
Siddharthaaa21 <siddharthaarora2131@gmail.com>
2024-02-14 21:17:50 +0530

    pushing code for division

--- Calculator/src/main/java/Calculator.java
+++ Calculator/src/main/java/Calculator.java
@@ -3,9 +3,14 @@ public class Calculator {
     public int addition(int a, int b)   {
         return a + b;
     }
+    public double division(int a, int b)   {
+        return a / b;
+    }
 
     public static void main(String[] args)  {
         Calculator c = new Calculator();
         c.addition(2, 3);
+        c.division(10, 2);
     }
+   
 }
