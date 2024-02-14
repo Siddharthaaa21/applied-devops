8718d787baa1af58c2ae139e5eeb7eb9dec4d9ce
Siddharthaaa21 <siddharthaarora2131@gmail.com>
2024-02-14 21:21:18 +0530

    pushing divvisio with a new branch

--- Calculator/src/main/java/Calculator.java
+++ Calculator/src/main/java/Calculator.java
@@ -3,9 +3,11 @@ public class Calculator {
     public int addition(int a, int b)   {
         return a + b;
     }
+
     public double division(int a, int b)   {
         return a / b;
     }
+    
 
     public static void main(String[] args)  {
         Calculator c = new Calculator();
